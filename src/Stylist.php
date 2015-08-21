7a69351c7fb68501209624da70416198e663de12
lujxio <jcdiaz92@gmail.com>
2015-08-21 12:41:15 -0700

    functions for Stylist class

--- src/Stylist.php
+++ src/Stylist.php
@@ -47,5 +47,50 @@
 			}
 			$GLOBALS['DB']->exec("DELETE FROM stylists WHERE id = {$this->getId()};");
 		}
+		
+		static function getAll()
+		{
+			$returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists;");
+			$stylists = array();
+			foreach($returned_stylists as $stylist) {
+				$name = $stylist['name'];
+				$id = $stylist['id'];
+				$new_stylist = new Stylist($name, $id);
+				array_push($stylists, $new_stylist);
+			}
+			return $stylist;
+		}
+		
+		static function deleteAll()
+		{
+			$GLOBALS['DB']->exec("DELETE FROM stylists;");
+		}
+		
+		static function find($search_id)
+		{
+			$found_stylist = null;
+			$stylists = Stylist::getAll();
+			foreach($stylists as $stylist) {
+				$stylist_id = $stylist->getId();
+				if($stylist_id == $search_id) {
+					$found_stylist = $stylist;
+				}
+			}
+			return $found_stylist;
+		}
+		
+		function getClients()
+		{
+			$clients = array();
+			$returned_clients = $GLOBALS['DB']->query("SELECT * FROM clients WHERE stylist_id = {$this->getId()};");
+			foreach($returned_clients as $client) {
+				$name = $client['name'];
+				$stylist_id = $client['stylist_id'];
+				$id = $client['id'];
+				$new_client = new Client($name, $stylist_id, $id);
+				array_push($clients, $new_client);
+			}
+			return $clients;
+		}
 	}
 ?>
