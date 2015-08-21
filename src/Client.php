101bdef9935f1500f597aaad37b7cb42b4279e87
lujxio <jcdiaz92@gmail.com>
2015-08-21 12:50:59 -0700

    write getters and setters for Client class

--- src/Client.php
+++ src/Client.php
@@ -1,3 +1,40 @@
 <?php 
-	
+	class Client
+	{
+		private $name;
+		private $stylist_id;
+		private $id;
+		
+		function __construct($name, $stylist_id, $id=null)
+		{
+			$this->name = $name;
+			$this->stylist_id = $stylist_id;
+			$this->id = $id;
+		}
+		
+		function setName($new_name)
+		{
+			$this->name = $new_name;
+		}
+		
+		function getName()
+		{
+			return $this->name;
+		}
+		
+		function setStylistId($new_stylist_id)
+		{
+			$this->stylist_id = $new_stylist_id;
+		}
+		
+		function getStylistId()
+		{
+			return $this->stylist_id;
+		}
+		
+		function getId()
+		{
+			return $this->id;
+		}
+	}
 ?>
